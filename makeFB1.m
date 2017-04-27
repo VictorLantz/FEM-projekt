855de06ab58f4fa59b51222c459ff51ea4e2fb17
Felix Agner <felix.agner@hotmail.com>
2017-04-27 17:06:48 +0200

    lade till kod för att skapa fb1 och fb2

---
+++ makeFB1.m
@@ -0,0 +1,36 @@
+fb1 = zeros(ndof, 1);
+
+%Hitta ränders index:
+indexL3v = find(edge(5, :) == 3 & edge(4, :) >= 2/3);
+indexL3h = find(edge(5, :) == 3 & edge(4, :) < 2/3);
+indexL4 = find(edge(5, :) == 4);
+
+%Hitta integral L3v
+for i = indexL3v(1) : indexL3v(end)
+   
+    node1 = edge(1, i); node2 = edge(2, i);
+    l = sqrt( (point(1, node1)-point(1, node2))^2 + (point(2, node1)-point(2, node2))^2);
+    fb1(node1) = fb1(node1) + qel * l / 2;
+    fb1(node2) = fb1(node2) + qel * l / 2;
+    
+end
+
+%Hitta integral L3h
+for i = indexL3h(1) : indexL3h(end)
+   
+    node1 = edge(1, i); node2 = edge(2, i);
+    l = sqrt( (point(1, node1)-point(1, node2))^2 + (point(2, node1)-point(2, node2))^2);
+    fb1(node1) = fb1(node1) - ac * Tinf * ksmd * l / 2;
+    fb1(node2) = fb1(node2) - ac * Tinf * ksmd * l / 2;
+    
+end
+
+%Hitta integral L4
+for i = indexL4(1) : indexL4(end)
+   
+    node1 = edge(1, i); node2 = edge(2, i);
+    l = sqrt( (point(1, node1)-point(1, node2))^2 + (point(2, node1)-point(2, node2))^2);
+    fb1(node1) = fb1(node1) - ac * Tinf * ksol * l / 2;
+    fb1(node2) = fb1(node2) - ac * Tinf * ksol * l / 2;
+    
+end
---
+++ makeFB2.m
@@ -0,0 +1,25 @@
+fb2 = zeros(ndof);
+
+%Hitta ränders index:
+indexL3h = find(edge(5, :) == 3 & edge(4, :) < 2/3);
+indexL4 = find(edge(5, :) == 4);
+
+%Hitta integral L3h
+for i = indexL3h(1) : indexL3h(end)
+   
+    node1 = edge(1, i); node2 = edge(2, i);
+    l = sqrt( (point(1, node1)-point(1, node2))^2 + (point(2, node1)-point(2, node2))^2);
+    fb2(node1, node2) = fb2(node1, node2) - ac * ksmd * l / 6;
+    fb2(node2, node1) = fb2(node2, node1) - ac * ksmd * l / 6;
+    
+end
+
+%Hitta integral L4
+for i = indexL4(1) : indexL4(end)
+   
+    node1 = edge(1, i); node2 = edge(2, i);
+    l = sqrt( (point(1, node1)-point(1, node2))^2 + (point(2, node1)-point(2, node2))^2);
+    fb2(node1, node2) = fb2(node1, node2) - ac * ksol * l / 2;
+    fb2(node2, node1) = fb2(node2, node1) - ac * ksol * l / 2;
+    
+end
